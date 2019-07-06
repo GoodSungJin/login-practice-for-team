0317699290e4f0aa0beb176866dc56c0d4cf3b5f
goodsungjin <mirepach@naver.com>
2019-07-07 01:22:46 +0900

    -ing

---
+++ ChoiSungJin/login/src/app/password-validator.ts
@@ -0,0 +1,11 @@
+import { AbstractControl } from '@angular/forms';
+
+export class PasswordValidator {
+  static match(passwordGroup: AbstractControl) {
+    const password = passwordGroup.get('password').value;
+    const confirmPassword = passwordGroup.get('confirmPassword').value;
+
+    if (password === confirmPassword) return null;
+    return { match: { password, confirmPassword } };
+  }
+}
